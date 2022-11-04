00ce2dea9e346d2baeea1f6976554f02ce5afe0d
YMS <ms.yuguda0@gmail.com>
2022-11-03 23:45:41 -1100

    created a maths function post request

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 user_dict = {"slackUsername": "yuguda999",
-        "backend": True,
-        "age": 23,
-        "bio": "Logical and result-driven programmer.I love to devise elaborate methods for solving general problems"
-        }
+             "backend": True,
+             "age": 23,
+             "bio": "Logical and result-driven programmer.I love to devise elaborate methods for solving general problems"
+             }
+
+
 @app.route("/user-info")
 def user():
     return jsonify(user_dict)
+
+
+@app.route("/math", methods=["POST"])
+def operation():
+    result=0
+    operation_type = request.args.get('operation_type')
+    x = int(request.args.get('x'))
+    y = int(request.args.get('y'))
+    if operation_type == 'addition':
+        result = x + y
+    elif operation_type == 'subtraction':
+        result = x - y
+    elif operation_type == 'multiplication':
+        result = x * y
+
+    return jsonify({"slackUsername": "yuguda999", "operation_type": operation_type, "result": result})
 
 
 if __name__ == "__main__":
